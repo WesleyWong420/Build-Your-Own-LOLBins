b3a32c3a69d50b2cac891a023993f5473667a8f0
JesusCries <wesleywongkeehan@gmail.com>
2022-12-28 11:18:20 +0800

    Added Docker Files

--- BYOL/settings.py
+++ BYOL/settings.py
@@ -136,8 +136,8 @@ MESSAGE_TAGS = {
 
 # Redis
 
-CELERY_BROKER_URL = 'redis://localhost:6379'
-CELERY_RESULT_BACKEND = "redis://localhost:6379"
+CELERY_BROKER_URL = 'redis://redis:6379/0'
+CELERY_RESULT_BACKEND = "redis://redis:6379/0"
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
